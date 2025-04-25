ceeb64fee26fe55f2b2ba50c25e7b1898d696d84
calleluis <lfcalle1@espe.edu.ec>
2025-04-25 09:22:10 -0500

    añadir funcion saludar

--- test.cpp
+++ test.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
+
 using namespace std;
 
 int main(){
 
-cout<<"Hola"<<endl;
-cout<<"Github<<endl;
+void saludar();
+
+saludar();
+cout<<"Github"<<endl;
 
 return 0;
 }
+void saludar(){
+cout<<"Hola"<<endl;
+
+}
+
+
+
